2dfc8f55f3de96bcf7c201a2e1f0dff3febc3031
XSC-en <2428433363@qq.com>
2026-09-27 03:18:17 +0800

    fix: add missing background and sp imports in QQLogPage

--- app/src/main/java/me/rerere/rikkahub/ui/pages/qqlog/QQLogPage.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/pages/qqlog/QQLogPage.kt
@@ -1,5 +1,6 @@
 package me.rerere.rikkahub.ui.pages.qqlog
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -10,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import me.rerere.rikkahub.ui.components.GlassCard
 import me.rerere.rikkahub.ui.theme.*
 
