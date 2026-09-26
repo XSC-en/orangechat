d6fc98bd525b3e81c5000bcdcfb58f057f42c78c
XSC-en <2428433363@qq.com>
2026-09-26 18:35:26 +0800

    chore: trigger CI build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+// muran: 暮色主题 fork of orangechat
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
